89c04bf0349a9af404f8365d4f53970ac2b8f66f
Roshan_Ghadge <roshanghadge20@gmail.com>
2024-11-24 19:35:02 +0530

    implented till slider value to 560

--- src/test/java/Base_Test/FitPeo_HomepageTest.java
+++ src/test/java/Base_Test/FitPeo_HomepageTest.java
@@ -8,46 +8,46 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class FitPeo_HomepageTest 
+public class Homepage 
 {
-	public static void main(String[] args) throws InterruptedException 
-	{
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-
+	public static void main(String[] args) throws InterruptedException {
 		
-		driver.get("https://www.fitpeo.com/");
-		driver.manage().window().maximize();
+	
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver = new ChromeDriver();
 
-		WebElement revenueCalButton = driver.findElement(By.xpath("//div[text()='Revenue Calculator']"));
-		revenueCalButton.click();
-		
+	
+	driver.manage().window().maximize();
+	driver.get("https://www.fitpeo.com/");
+	
+	driver.navigate().to("https://www.fitpeo.com/revenue-calculator");
+	Thread.sleep(2000);
+	
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	js.executeScript("window.scrollBy(0,600);");
+	
 
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,500);");
-		
+	//slider to 820 value
+	WebElement slider = driver.findElement(By.xpath("//input[@type='range']")); 
 
-		//slider to 820 value
-		WebElement slider = driver.findElement(By.cssSelector("input[type='range']")); // Replace selector if needed
-
-		int targetValue = 820; 
-		js.executeScript("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('input'));", slider,
-				targetValue);
-		String sliderValue = slider.getAttribute("value");
-		System.out.println("Current slider value: " + sliderValue);
-
-		if (sliderValue.equalsIgnoreCase("820")) 
-		{
-			assert sliderValue.equals(String.valueOf(targetValue)) : "Slider value mismatch. Expected: " + targetValue;
-			System.out.println("Slider Values Matches");
-		}
-		else {
-			System.out.println("Slider Value Doesn't Match as expected");
-		}
-		
-		
-		
-		driver.quit();
-
-	}
+	int targetValue = 820; 
+	js.executeScript("arguments[0].value = arguments[1]; arguments[0].dispatchEvent(new Event('input'));", slider,
+			targetValue);
+	Thread.sleep(2000);
+	String sliderValue = slider.getAttribute("value");
+	System.out.println("Current slider value: " + sliderValue);
+	
+	//Slider to 560 value 
+	WebElement text_field= driver.findElement(By.cssSelector("input[id=':R57alklff9da:']"));
+	text_field.clear();
+	Thread.sleep(2000);
+	text_field.sendKeys("5");
+	Thread.sleep(2000);
+	String sliderValue_updated = slider.getAttribute("value");
+	System.out.println("Current slider value: " + sliderValue_updated);
+	
+	
+	driver.quit();
+}
+	
 }
